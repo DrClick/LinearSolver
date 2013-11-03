cd743e5a597180b50aff814d58dd92923eb43738
Thomas Watson <drclick@mac.com>
2013-11-03 09:58:58 -0400

    used the __main__ do properly execute as script

--- Part2.py
+++ Part2.py
@@ -1,8 +1,12 @@
 __author__ = 'thomaswatson'
 from SimplexDictionary import SimplexDictionary
-
+import sys
 
 def pivot_dictionary_until_final(filename):
+    """
+    Pivots the dictionary until it is final and reports the final objective value and number of pivots
+    used to find the final dictionary
+    """
     my_dictionary = SimplexDictionary.parse_from_file(filename)
 
     result = "PIVOTING"
@@ -45,6 +49,16 @@ def pivot_dictionary_until_final(filename):
 
 
 #For all the glory, solve the dictionaries
-for i in range(1,6):
-    print "Dict{0}".format(i)
-    pivot_dictionary_until_final("/Users/thomaswatson/code/coursera/linear/LinearSolver/dicts/part2TestCases/assignmentParts/part{0}.dict".format(i))
+
+def main():
+    """
+    Will pivot all the dictionaries in the assignment
+    """
+    for i in range(1,6):
+        print "Dict{0}".format(i)
+        pivot_dictionary_until_final("/Users/thomaswatson/code/coursera/linear/LinearSolver/dicts/part2TestCases/assignmentParts/part{0}.dict".format(i))
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
